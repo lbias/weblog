1d2c3e2eaa3f480cd9a4d5feccffe2dc49ed79c7
lbias <306508354@qq.com>
2017-06-05 23:02:01 +0800

    add links of my blog in welcome index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
     redirect_to @article
   end
@@ -14,10 +14,11 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-  end  
+  end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
